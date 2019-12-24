bcdf0f6b134c1c45089943baae2ce4c9b7fa2ade
Umran Hussein <umran.hussein@gmail.com>
2019-12-23 19:28:51 -0800

    completed day 5 part 2

--- five/main.go
+++ five/main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+// Computer ...
+type Computer struct {
+	pointer      int
+	instructions []int
+}
+
 func parseInstructions(filename string) []int {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -25,7 +31,7 @@ func parseInstructions(filename string) []int {
 	return intArray
 }
 
-func determineOperation(code int) (op int, modes []int) {
+func determineOperationAndModes(code int) (op int, modes []int) {
 	codeString := strconv.FormatInt(int64(code), 10)
 	slices := strings.Split(codeString, "")
 
@@ -45,98 +51,161 @@ func determineOperation(code int) (op int, modes []int) {
 	return op, modes
 }
 
-func runProgram(instructions []int, input int) []int {
-	pointer := 0
+func normalizeModes(modes []int, lenParams int) []int {
+	fixedModes := make([]int, lenParams)
+	copy(fixedModes, modes)
+
+	return fixedModes
+}
+
+func (c *Computer) initializeProgram(instructions []int) {
+	// we create a copy of the instructions to prevent mutating the original
+	ownedInstructions := make([]int, len(instructions))
+	copy(ownedInstructions, instructions)
+
+	c.pointer = 0
+	c.instructions = ownedInstructions
+}
+
+func (c *Computer) nextPointer(value int) {
+	c.pointer = value
+}
+
+func (c *Computer) resolveValues(params, modes []int) []int {
+	modes = normalizeModes(modes, len(params))
+	values := make([]int, len(params))
+
+	for i, param := range params {
+		switch modes[i] {
+		case 0:
+			values[i] = c.instructions[param]
+		default:
+			values[i] = param
+		}
+	}
+
+	return values
+}
+
+func (c *Computer) add() {
+	defer c.nextPointer(c.pointer + 4)
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+4]
+	values := c.resolveValues(params, modes)
+	c.instructions[params[2]] = values[0] + values[1]
+}
+
+func (c *Computer) mul() {
+	defer c.nextPointer(c.pointer + 4)
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+4]
+	values := c.resolveValues(params, modes)
+	c.instructions[params[2]] = values[0] * values[1]
+}
+
+func (c *Computer) set(input int) {
+	defer c.nextPointer(c.pointer + 2)
+
+	params := c.instructions[c.pointer+1 : c.pointer+2]
+	c.instructions[params[0]] = input
+}
+
+func (c *Computer) get() int {
+	defer c.nextPointer(c.pointer + 2)
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+2]
+	values := c.resolveValues(params, modes)
+
+	return values[0]
+}
+
+func (c *Computer) jumpIfTrue() {
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+3]
+	values := c.resolveValues(params, modes)
+
+	switch {
+	case values[0] != 0:
+		c.nextPointer(values[1])
+	default:
+		c.nextPointer(c.pointer + 3)
+	}
+}
+
+func (c *Computer) jumpIfFalse() {
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+3]
+	values := c.resolveValues(params, modes)
+
+	switch {
+	case values[0] == 0:
+		c.nextPointer(values[1])
+	default:
+		c.nextPointer(c.pointer + 3)
+	}
+}
+
+func (c *Computer) lessThan() {
+	defer c.nextPointer(c.pointer + 4)
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+4]
+	values := c.resolveValues(params, modes)
+	switch values[0] < values[1] {
+	case true:
+		c.instructions[params[2]] = 1
+	default:
+		c.instructions[params[2]] = 0
+	}
+}
+
+func (c *Computer) equals() {
+	defer c.nextPointer(c.pointer + 4)
+	_, modes := determineOperationAndModes(c.instructions[c.pointer])
+
+	params := c.instructions[c.pointer+1 : c.pointer+4]
+	values := c.resolveValues(params, modes)
+	switch values[0] == values[1] {
+	case true:
+		c.instructions[params[2]] = 1
+	default:
+		c.instructions[params[2]] = 0
+	}
+}
+
+func (c *Computer) runProgram(instructions []int, arg int) []int {
+	c.initializeProgram(instructions)
 	log := make([]int, 0)
 
 program:
 	for {
-		opCode, modes := determineOperation(instructions[pointer])
+		opCode, _ := determineOperationAndModes(c.instructions[c.pointer])
 
 		switch opCode {
 		case 0:
-			pointer++
+			c.nextPointer(c.pointer + 1)
 		case 1:
-			fmt.Println("detected an addition operation")
-			fixedModes := make([]int, 3)
-			copy(fixedModes, modes)
-
-			var (
-				a int
-				b int
-			)
-
-			switch fixedModes[0] {
-			case 0:
-				a = instructions[instructions[pointer+1]]
-			default:
-				a = instructions[pointer+1]
-			}
-
-			switch fixedModes[1] {
-			case 0:
-				b = instructions[instructions[pointer+2]]
-			default:
-				b = instructions[pointer+2]
-			}
-
-			result := a + b
-			instructions[instructions[pointer+3]] = result
-
-			pointer += 4
+			c.add()
 		case 2:
-			fmt.Println("detected a multiplication operation")
-			fixedModes := make([]int, 3)
-			copy(fixedModes, modes)
-
-			var (
-				a int
-				b int
-			)
-
-			switch fixedModes[0] {
-			case 0:
-				a = instructions[instructions[pointer+1]]
-			default:
-				a = instructions[pointer+1]
-			}
-
-			switch fixedModes[1] {
-			case 0:
-				b = instructions[instructions[pointer+2]]
-			default:
-				b = instructions[pointer+2]
-			}
-
-			result := a * b
-			instructions[instructions[pointer+3]] = result
-
-			pointer += 4
+			c.mul()
 		case 3:
-			fmt.Println("detected a set operation")
-			// fixedModes := make([]int, 1)
-			// copy(fixedModes, modes)
-			instructions[instructions[pointer+1]] = input
-
-			pointer += 2
+			c.set(arg)
 		case 4:
-			fmt.Println("detected a get operation")
-			fixedModes := make([]int, 1)
-			copy(fixedModes, modes)
-
-			var getPointer int
-			switch fixedModes[0] {
-			case 0:
-				getPointer = instructions[pointer+1]
-			default:
-				getPointer = pointer + 1
-			}
-
-			log = append(log, instructions[getPointer])
-
-			pointer += 2
+			log = append(log, c.get())
+		case 5:
+			c.jumpIfTrue()
+		case 6:
+			c.jumpIfFalse()
+		case 7:
+			c.lessThan()
+		case 8:
+			c.equals()
 		case 99:
-			fmt.Println("detected a halt instruction")
 			break program
 		}
 	}
@@ -146,6 +215,14 @@ program:
 
 func main() {
 	instructions := parseInstructions("./instructions.txt")
-	log := runProgram(instructions, 1)
+	computer := new(Computer)
+	log := computer.runProgram(instructions, 1)
+
+	// this is the answer to part one
 	fmt.Println(log)
+
+	log2 := computer.runProgram(instructions, 5)
+
+	// this is the answer to part two
+	fmt.Println(log2)
 }
